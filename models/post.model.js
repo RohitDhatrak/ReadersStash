430e7cce707d2777d4308487a49879504df05861
Rohit Dhatrak <57338980+rohit436@users.noreply.github.com>
2021-12-20 12:23:13 +0530

    added post model

---
+++ models/post.model.js
@@ -0,0 +1,38 @@
+const { model, Schema } = require("mongoose");
+
+const PostSchema = new Schema(
+    {
+        title: { type: String, required: [true, "Title is required"] },
+        body: { type: String, required: [true, "Body is required"] },
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: [true, "User id is required"],
+        },
+        likes: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Comment",
+            },
+        ],
+        topics: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Topic",
+            },
+        ],
+        createdAt: Number,
+        updatedAt: Number,
+    },
+    { timestamps: true }
+);
+
+const Post = model("Post", PostSchema);
+
+module.exports = { Post };
