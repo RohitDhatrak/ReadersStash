64aed363f011b5ab4d0bf2fba4b0819fa7c2f942
Rohit Dhatrak <57338980+rohit436@users.noreply.github.com>
2022-01-20 22:06:15 +0530

    feed and explore pages

--- client/src/features/posts/Explore/Explore.tsx
+++ client/src/features/posts/Explore/Explore.tsx
@@ -7,18 +7,18 @@ import { FlexContainer, Container } from "../../../components/Shared";
 import { InputBox, ActionButton, Post } from "../../../components";
 import { GET_POSTS } from "../../../graphql/queries";
 import { loaded, getPosts } from "../postsSlice";
+import { PageContainer } from "./style.explore";
 
 export function Explore() {
-    const dispatch = useAppDispatch();
     const posts = useAppSelector(getPosts);
 
-    useQuery(GET_POSTS, {
-        onCompleted(data) {
-            if (posts.length === 0) {
-                dispatch(loaded(data.getPosts));
-            }
-        },
-    });
-
-    return <Container>Explore</Container>;
+    return (
+        <PageContainer mb="2em" justify="center">
+            <FlexContainer direction="column" align="center">
+                {posts.map((post: PostType) => (
+                    <Post post={post} key={post._id} />
+                ))}
+            </FlexContainer>
+        </PageContainer>
+    );
 }
--- client/src/features/posts/Feed/Feed.tsx
+++ client/src/features/posts/Feed/Feed.tsx
@@ -3,15 +3,18 @@ import { Link, useNavigate, useLocation } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { InputEvent, FormEvent, Post as PostType } from "../../../types";
 import { useAppSelector, useAppDispatch } from "../../../app/hooks";
-import { FlexContainer, Container } from "../../../components/Shared";
+import { FlexContainer, Container, Image } from "../../../components/Shared";
 import { InputBox, ActionButton, Post } from "../../../components";
 import { GET_POSTS } from "../../../graphql/queries";
 import { loaded, getPosts } from "../postsSlice";
 import { PageContainer } from "./style.feed";
+import checkIcon from "../../../assets/checked.png";
+import { getUser } from "../../user/userSlice";
 
 export function Feed() {
     const dispatch = useAppDispatch();
     const posts = useAppSelector(getPosts);
+    const user = useAppSelector(getUser);
 
     useQuery(GET_POSTS, {
         onCompleted(data) {
@@ -24,9 +27,41 @@ export function Feed() {
     return (
         <PageContainer mb="2em" justify="center">
             <FlexContainer direction="column" align="center">
-                {posts.map((post: PostType) => (
-                    <Post post={post} key={post._id} />
-                ))}
+                {posts.map((post: PostType) =>
+                    user.followingHashMap?.[post.user._id] ? (
+                        <Post post={post} key={post._id} />
+                    ) : null
+                )}
+                <FlexContainer
+                    direction="column"
+                    textAlign="center"
+                    w="100%"
+                    bgc="var(--card-color)"
+                    pb="1em"
+                >
+                    <Container
+                        w="100%"
+                        h="0.3em"
+                        bgc="var(--primary-color)"
+                        color="transparent"
+                    >
+                        .
+                    </Container>
+                    <Image
+                        src={checkIcon}
+                        w="3em"
+                        m="0 auto"
+                        mt="1em"
+                        mb="1em"
+                    />
+                    <Container fs="1.1rem" mb="0.5em">
+                        You are all caught up!
+                    </Container>
+                    <Container fw={500} m="0 1.5em">
+                        Empty Feed? Discover new content and people in the
+                        explore section.
+                    </Container>
+                </FlexContainer>
             </FlexContainer>
         </PageContainer>
     );
